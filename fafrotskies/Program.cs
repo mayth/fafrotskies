fd5f1ebbd52d2255c136f871dc858b567a8ff577
mayth <chimeaquas@hotmail.com>
2014-03-15 12:45:37 +0900

    Add initial project files.

---
+++ fafrotskies/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fafrotskies
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
