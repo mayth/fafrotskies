ee21050398d356e361091edc1b517b466ec71fe0
mayth <chimeaquas@hotmail.com>
2014-03-17 03:57:24 +0900

    Add server, and communicate with the client.

--- fafrotskies/Program.cs
+++ fafrotskies/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using YamlDotNet.Serialization;
 
 namespace fafrotskies
 {
@@ -7,27 +6,34 @@ namespace fafrotskies
     {
         public static void Main(string[] args)
         {
-            if (args.Length == 0)
+            int port = 3939;
+            var optParser = new NDesk.Options.OptionSet()
+            {
+                { "port=", v => port = int.Parse(v) }
+            };
+            var remain = optParser.Parse(args);
+
+            if (remain.Count == 0)
             {
                 Console.WriteLine("give me a path to the problem file!");
                 return;
             }
-            var problemPath = args[0];
+            var problemPath = remain[0];
             if (!System.IO.File.Exists(problemPath))
             {
                 Console.WriteLine("The specified path '{0}' is not found.", problemPath);
                 return;
             }
             var problem = Problem.Load(problemPath);
+
             Console.WriteLine("Name: {0}", problem.Name);
             Console.WriteLine("Desc: {0}", problem.Description);
             Console.WriteLine("Flag: {0}", problem.Flag);
-            Console.WriteLine("Cases:");
-            foreach (var pCase in problem.Cases)
-            {
-                Console.WriteLine("  Limit: {0}", pCase.Limit);
-                Console.WriteLine("  Problem: {0}", pCase.Problem);
-            }
+            Console.WriteLine("Port: {0}", port);
+            var server = new Server(port, problem);
+            server.Start();
+            while (!Console.KeyAvailable) { }
+            server.Stop();
         }
     }
 }
---
+++ fafrotskies/Server.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace fafrotskies
+{
+    public class Server
+    {
+        private int port;
+        private Problem problem;
+        private bool isRunning;
+
+        public Server(int port, Problem problem)
+        {
+            this.port = port;
+            this.problem = problem;
+            isRunning = false;
+        }
+
+        public void Start()
+        {
+            var listener = new TcpListener(System.Net.IPAddress.Any, port);
+            listener.Start();
+            isRunning = true;
+            Task.Factory.StartNew(
+                () =>
+                {
+                    while (isRunning)
+                    {
+                        var client = listener.AcceptTcpClient();
+                        Console.WriteLine("accepted.");
+                        Task.Factory.StartNew(Communicate, client);
+                    }
+                });
+        }
+
+        public void Stop()
+        {
+            isRunning = false;
+        }
+
+        void Communicate(object obj)
+        {
+            var client = (TcpClient)obj;
+            var isCleared = true;
+            using (client)
+            using (var stream = client.GetStream())
+            using (var writer = new System.IO.StreamWriter(stream))
+            using (var reader = new System.IO.StreamReader(stream))
+            {
+                writer.WriteLine("Welcome. We are the fafrotskies.");
+                writer.WriteLine();
+                writer.WriteLine("===== {0} =====", problem.Name);
+                writer.WriteLine(problem.Description);
+                writer.WriteLine();
+                writer.Flush();
+                foreach (var c in problem.Cases.Select((v, i) => new {Index = i, Case = v}))
+                {
+                    writer.WriteLine("#{0}", c.Index + 1);
+                    writer.WriteLine(c.Case.Problem);
+                    writer.Flush();
+                    var answer = reader.ReadLine();
+                    if (!c.Case.Check(answer))
+                    {
+                        isCleared = false;
+                        break;
+                    }
+                }
+                if (isCleared)
+                {
+                    writer.WriteLine("Congratulations!");
+                    writer.WriteLine("Flag is {0}", problem.Flag);
+                }
+                else
+                {
+                    writer.WriteLine("Oops...");
+                }
+                writer.Flush();
+            }
+        }
+    }
+}
+
